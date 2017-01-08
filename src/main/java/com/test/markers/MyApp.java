98b3639df7757067b09f17798edf758a33f96f36
Adeeti Kaushal <akaush1@m-C02LT2NAFD59.local>
2017-01-07 22:35:29 -0800

    call from Db

--- src/main/java/com/test/markers/MyApp.java
+++ src/main/java/com/test/markers/MyApp.java
@@ -1,6 +1,5 @@
 package com.test.markers;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,17 +8,44 @@ import javax.sql.DataSource;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.test.db.BiomarkerRepository;
+import com.test.db.ProcessRepository;
+
+
+
+
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.test.db"})
+@EnableAutoConfiguration
+@EnableJpaRepositories(basePackages="com.test.db")
+@EntityScan(basePackages="com.test.db")
+
+
 public class MyApp {
 	
+
+//	
+	@Autowired
+	ApplicationContext ctx;
+	
+	//@Autowired
+	private BiomarkerRepository biomarkerRepository;
+	
+	private ProcessRepository processRepository;
 	
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
@@ -37,27 +63,27 @@ public class MyApp {
 //		};
 //    }
     
+   
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
+//
+//        };
+//    }
+//   
+    
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-    
-    
-    
-    //@Bean
     CommandLineRunner init( final RepositoryService repositoryService,
                                   final RuntimeService runtimeService,
-                                  final TaskService taskService) {
+                                  final TaskService taskService ) {
 
         return new CommandLineRunner() {
 
@@ -65,10 +91,15 @@ public class MyApp {
                 Map<String, Object> variables = new HashMap<String, Object>();
                 variables.put("markerName", "Test");
                 variables.put("temperature", "90");
-//                for (Process process1 : repo.findAll()){
-//                	System.out.println("------------> " + process1.toString());
+                biomarkerRepository = (BiomarkerRepository) ctx.getBean("biomarkerRepository");
+//                for(Biomarker makers : biomarkerRepository.findAll() ){
+//               	System.out.println("-------" + makers.toString() );
+//                }
+                processRepository = (ProcessRepository) ctx.getBean("processRepository");
+                for (com.test.db.Process process1 : processRepository.findAll()){
+                	System.out.println("------------> " + process1.toString());
                 	runtimeService.startProcessInstanceByKey("temperaturecropprocess", variables);
-              //  }
+                }
             }
         };
     }
